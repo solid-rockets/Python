1258f98abfa94f2feb590b37ef7fad3a34647a76
AMF <af.dev@protonmail.com>
2025-12-31 19:09:03 +0900

    feat: split out the common logic

--- kioku/main.py
+++ kioku/main.py
@@ -1,89 +1,16 @@
-import sys
-import random
-
-# Constants
-MAX_IN_TEST = 20
-
-# Logic
-def convert_into_line(card):
-  return f"{card['front']} : {card['back']} : {card['score']}"
-
-def convert_into_card(line):
-  fields = line.split(":")
-
-  front = fields[0].strip()
-  back = fields[1].strip()
-  score = 0 if len(fields) <= 2 else int(fields[2].strip())
-
-  return {
-    "type": "card",
-    "front": front,
-    "back": back,
-    "score": score
-  }
-
-# Read all lines and convert them into cards.
-def read_deck_from_path(deck_path):
-  cards = []
-
-  with open(deck_path, "r") as f:
-    for line in f:
-      cards.append(convert_into_card(line))
-
-  return cards
-
-def write_deck_to_path(deck_path, deck):
-  with open(deck_path, "w") as f:
-    for card in deck:
-      print(convert_into_line(card), file=f)
-
-def get_sorted_scores(deck):
-  scores = [card["score"] for card in deck]
-  scores = list(set(scores))
-  scores.sort()
-
-  return scores
-
-def get_test_cards(deck):
-  # Get a list of scores, ordered from worst to best.
-  scores = get_sorted_scores(deck)
-  deck = random.sample(deck, len(deck))
-  test_deck = []
-
-  for score in scores:
-    for card in deck:
-      if card["score"] == score:
-        test_deck.append(card)
-
-      if len(test_deck) == MAX_IN_TEST:
-        return test_deck
-
-  # This handles cases when the deck is smaller than MAX_IN_TEST
-  return test_deck
-
-def reset_cards(deck):
-  min_score = get_sorted_scores(deck)[0]
-  for card in deck:
-    card["score"] = card["score"] - min_score
-
-def get_deck_path():
-  if len(sys.argv) < 2:
-    print("Please provide path to deck.")
-    sys.exit()
-  else:
-    return sys.argv[1]
+import common as c
 
 # Main logic
 is_running = True
-deck_path = get_deck_path()
-full_deck = read_deck_from_path(deck_path)
+deck_path = c.get_deck_path()
+full_deck = c.read_deck_from_path(deck_path)
 
 while is_running:
-  test_deck = get_test_cards(full_deck)
+  test_deck = c.get_test_cards(full_deck)
 
   for card in test_deck:
     input(card["front"])
-    key = input(card["back"])
+    key = input(card["back"] + " ")
 
     match(key):
       case("y"):
@@ -93,6 +20,6 @@ while is_running:
       case _:
         is_running = False
         break
-  reset_cards(full_deck)
+  c.reset_cards(full_deck)
 
-write_deck_to_path(deck_path, full_deck)
+c.write_deck_to_path(deck_path, full_deck)
