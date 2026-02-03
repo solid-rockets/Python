c0d2a4c220140880e10fd9c96d49a068fc826b36
AMF <af.dev@protonmail.com>
2026-02-03 22:47:02 +0900

    kioku: changed the format of adding cards

--- kioku/add.py
+++ kioku/add.py
@@ -4,19 +4,32 @@ import common as c
 is_running = True
 deck_path = c.get_deck_path()
 full_deck = c.read_deck_from_path(deck_path)
+total_num = len(full_deck)
 
 while is_running:
-  front_text = input("Front: ")
-  back_text = input("Back: ")
+  full_text = input(f"{total_num}: ")
 
-  if back_text == "!exit":
+  if full_text == "!exit":
     is_running = False
   else:
-    card = {}
-    card["front"] = front_text
-    card["back"] = back_text
-    card["negs"] = 0
-    card["score"] = 0
-    full_deck.append(card)
+    try:
+      full_text = full_text.replace("：", ":").replace("；", ";")
+      parts = list(map(str.strip, full_text.split(";")))
+
+      front_text = parts[0]
+      back_text = ";".join(parts[1:])
+
+      card = {}
+      card["front"] = front_text
+      card["back"] = back_text
+      card["negs"] = 0
+      card["score"] = 0
+
+      full_deck.append(card)
+      total_num = len(full_deck)
+    except Exception as e:
+      print("Enter both front and back separated by a semicolon")
+      print("problem ; definition")
+
 
 c.write_deck_to_path(deck_path, full_deck)
